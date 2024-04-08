734ce5b0cc602b39007201f38c1b3b56ae10f7b1
Paul Jimenez <pjf2180@me.com>
2024-04-08 11:38:36 -0700

    Added featured projects

---
+++ src/app/components/featured-project/featured-project.component.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+interface FeaturedProjectProps {
+  projectId: string;
+  projectName: string;
+  description: string;
+  imgSrc: string;
+}
+export function FeaturedProject({
+  projectId,
+  projectName,
+  description,
+  imgSrc,
+}: FeaturedProjectProps) {
+  return (
+    <div className="">
+      <div className="w-full h-[350px] relative border-[1px] border-b-0">
+        <Image src={imgSrc} alt="Crown Clothing Home Page" fill priority />
+      </div>
+      <div className="bg-white border-[1px] h-[250px] p-3">
+        <h3 className="font-bold text-xl my-4">{projectName}</h3>
+        <p>{description}</p>
+      </div>
+      <div className="flex justify-center border-[1px] border-t-0">
+        <button className="w-full p-3 bg-black text-white">Learn More</button>
+      </div>
+    </div>
+  );
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,23 @@
+import { CardGallery } from "../components/card-gallery/card-gallery.component";
+import { FeaturedProject } from "../components/featured-project/featured-project.component";
+import { listProjects } from "../lib/data/projects/listProjects";
 
-export default function ProjectsPage() {
+export default async function ProjectsPage() {
+  const featuredProjects = await listProjects();
+
   return (
-    <div className="h-full pt-20 px-8">
-     
+    <div className="h-full pt-40 px-8">
+      <CardGallery>
+        {featuredProjects.map((p) => (
+          <FeaturedProject
+            key={p.projectId}
+            projectId={p.projectId}
+            description={p.description}
+            projectName={p.projectName}
+            imgSrc={p.imgSrc}
+          />
+        ))}
+      </CardGallery>
     </div>
   );
-}
-
-function ProjectFeatureItem() {
-  return <div></div>
 }
