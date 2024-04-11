de41f9638f0eb3e496e5742dffbe1b93814cfba1
Paul Jimenez <pjf2180@me.com>
2024-04-11 14:28:32 -0700

    Improves nav bar styling

--- src/app/components/navbar/navbar.component.tsx
+++ src/app/components/navbar/navbar.component.tsx
@@ -1,11 +1,17 @@
-import styles from "./navbar.module.scss";
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface MenuItem {
   href: string;
   displayText: string;
 }
 export function Navbar() {
+  const pathname = usePathname();
+  const styleOne: string = "text-black";
+  const styleTwo: string = "border-[#f8f8f8] text-white";
+  const colorStyles = pathname === "/" ? styleTwo : styleOne;
+
   const menuItems: MenuItem[] = [
     {
       displayText: "Home",
@@ -27,10 +33,17 @@ export function Navbar() {
   return (
     <div className="flex p-8 z-[1] relative">
       <ul className="flex">
-        {menuItems.map((menuItem: MenuItem,idx) => {
+        {menuItems.map((menuItem: MenuItem, idx) => {
+          const isActive: boolean = pathname === menuItem.href;
+          const isActiveStyle = isActive ? "font-semibold" : "font-medium";
           return (
-            <li className="border-r-[1px] border-[#d8d8d8] text-[#d8d8d8] font-extralight text-xs p-2 pr-4 pl-4 leading-tight">
-              <Link href={menuItem.href}> 0{idx+1} : {menuItem.displayText}</Link>
+            <li
+              className={`border-r-[1px] text-sm py-2 px-4 leading-tight ${colorStyles} ${isActiveStyle}`}
+            >
+              <Link href={menuItem.href}>
+                {" "}
+                0{idx + 1} : {menuItem.displayText}
+              </Link>
             </li>
           );
         })}
