3cd5f2a956c08e0fc98c28d64555035ff52b12a0
Paul Jimenez <pjf2180@me.com>
2024-04-11 12:32:06 -0700

    Changed tech stack section to use chip component

--- src/app/components/project-detail/project-detail.component.tsx
+++ src/app/components/project-detail/project-detail.component.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { ProjectDetails } from "@/app/lib/data/projects/getProject";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { Chip } from "../chip/chip.component";
 
 interface ProjectDetailProps {
   details: ProjectDetails;
@@ -18,19 +19,18 @@ export default function ProjectDetail({ details }: ProjectDetailProps) {
           <div className="border-b-2 border-black pb-2 mb-4">
             <h1 className="text-3xl font-bold">{headingText}</h1>
           </div>
-          <h2 className="text-lg mb-2">Tech Stack</h2>
-          <ul>
+          <ul className="flex flex-wrap gap-2">
             {techStackNames.map((name: string) => (
-              <li key={name} className="text-xl">{name}</li>
+              <li key={name} className="text-white text-md whitespace-no-wrap">
+                <Chip>{name}</Chip>
+              </li>
             ))}
           </ul>
           <p className="pt-8 text-2xl">{projectDescription}</p>
         </div>
       </div>
       <div className="w-full lg:w-1/2 max-lg:p-3 pl-2">
-        <ResponsiveMasonry
-          columnsCountBreakPoints={{ 0: 2,  }}
-        >
+        <ResponsiveMasonry columnsCountBreakPoints={{ 0: 2 }}>
           <Masonry gutter="20px">
             {imageSources.map((imageSource) => {
               return (
