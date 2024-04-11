f3cd38c0c3f823aef492b725c2ada5c97d124cd8
Paul Jimenez <pjf2180@me.com>
2024-04-11 10:03:04 -0700

    Added padding for smaller than lg screens

--- src/app/components/project-detail/project-detail.component.tsx
+++ src/app/components/project-detail/project-detail.component.tsx
@@ -13,7 +13,7 @@ export default function ProjectDetail({ details }: ProjectDetailProps) {
 
   return (
     <div className="flex justify-center flex-wrap">
-      <div className="w-full md:w-1/2 p-4">
+      <div className="w-full lg:w-1/2">
         <div className="bg-gray-200 p-8">
           <div className="border-b-2 border-black pb-2 mb-4">
             <h1 className="text-3xl font-bold">{headingText}</h1>
@@ -27,7 +27,7 @@ export default function ProjectDetail({ details }: ProjectDetailProps) {
           <p className="pt-8 text-2xl">{projectDescription}</p>
         </div>
       </div>
-      <div className="w-full md:w-1/2 p-4">
+      <div className="w-full lg:w-1/2 max-lg:p-3 pl-2">
         <ResponsiveMasonry
           columnsCountBreakPoints={{ 0: 2,  }}
         >
