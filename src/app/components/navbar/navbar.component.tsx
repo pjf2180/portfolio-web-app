1ca2550ce95d1b29b468641cee359b2351c04191
Paul Jimenez <pjf2180@me.com>
2024-04-16 15:36:40 -0700

    Adds mobile menu capability

--- src/app/components/navbar/navbar.component.tsx
+++ src/app/components/navbar/navbar.component.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { MenuIcon } from "../menu-icon/menu-icon.component";
+import { useEffect, useState } from "react";
 
 interface MenuItem {
   href: string;
@@ -8,10 +10,37 @@ interface MenuItem {
 }
 export function Navbar() {
   const pathname = usePathname();
-  const styleOne: string = "text-black";
+  const styleOne: string = "text-white sm:text-black";
   const styleTwo: string = "border-[#f8f8f8] text-white";
   const colorStyles = pathname === "/" ? styleTwo : styleOne;
-  
+  const [menuIsOpen, setMenuIsOpen] = useState(true);
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 640px)");
+    const handleResize = () => {
+      setMenuIsOpen(!mediaQuery.matches);
+      setIsMobile(mediaQuery.matches);
+    }
+
+    handleResize();
+    mediaQuery.addEventListener("change", handleResize);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleResize);
+    };
+  }, []);
+
+  const handleMenuIconClick = () => {
+    setMenuIsOpen((x) => !x);
+  };
+
+  const handleNavOptionClick = () => {
+    if (isMobile) {
+      setMenuIsOpen(false);
+    }
+  };
+
   const menuItems: MenuItem[] = [
     {
       displayText: "Home",
@@ -27,15 +56,26 @@ export function Navbar() {
     },
   ];
   return (
-    <div className="flex p-8 z-[1] relative">
-      <ul className="flex">
+    <div className="flex flex-col z-[1] justify-end sm:justify-start py-4 sm:py-8 fixed top-0 left-0 right-0 sm:static bg-black sm:bg-transparent">
+      <div className="sm:hidden flex justify-end px-4 bg-black">
+        <button onClick={handleMenuIconClick}>
+          <MenuIcon />
+        </button>
+      </div>
+      <ul
+        className={`${
+          !menuIsOpen ? "hidden" : ""
+        } flex flex-col sm:flex-row mt-4`}
+      >
         {menuItems.map((menuItem: MenuItem, idx) => {
-          const firstSegment: string = pathname.split('/')[1];
+          const firstSegment: string = pathname.split("/")[1];
           const isActive: boolean = firstSegment === menuItem.href.slice(1);
           const isActiveStyle = isActive ? "font-semibold" : "font-medium";
           return (
             <li
-              className={`border-r-[1px] text-sm py-2 px-4 leading-tight ${colorStyles} ${isActiveStyle}`}
+              key={menuItem.displayText}
+              className={`max-sm:border-b-[1px] py-4 sm:border-r-[1px] text-right text-sm  px-4 leading-tight ${colorStyles} ${isActiveStyle} `}
+              onClick={() => handleNavOptionClick()}
             >
               <Link href={menuItem.href}>
                 {" "}
