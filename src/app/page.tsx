6ad49d808a98a82e111ebc586a54f03e7a302784
Paul Jimenez <pjf2180@me.com>
2024-04-16 16:39:58 -0700

    Ordered tailwind utilities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Header } from "./components/header/header.component";
 
 export default function Home() {
   return (
-    <div className="absolute top-0 z-[-1] pt-16 sm:pt-0 left-0 right-0 mx-4 px-4 h-full flex flex-col justify-center">
+    <div className="absolute top-0 left-0 right-0 z-[-1] px-4 pt-16 mx-4 h-full flex flex-col justify-center sm:pt-0">
       <Background />
       <Header text="Paul Jiménez" />
       <Header typeAnimation text="Full Stack Software Engineer" />
